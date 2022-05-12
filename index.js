a799a4ba4bbcfc8548bbd7a41a3ee43d3996b16f
Juan-Cruz-Pares <paresjuancruz@gmail.com>
2022-05-12 11:51:05 -0300

    pruebas

---
+++ index.js
@@ -0,0 +1,14 @@
+const pdf = require("html-pdf");
+const fs = require("fs");
+const ubicacionPlantilla = require.resolve("./plantilla.html");
+let contenidoHtml = fs.readFileSync(ubicacionPlantilla, 'utf8')
+const valorPasadoPorNode = "tuki";
+contenidoHtml = contenidoHtml.replace("{{valor}}", valorPasadoPorNode);
+
+pdf.create(contenidoHtml).toFile("salida.pdf", (error) => {
+    if (error) {
+        console.log("Error creando PDF: " + error)
+    } else {
+        console.log("PDF creado correctamente");
+    }
+});
